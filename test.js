afb8ef2b9d875802ea1d9db98e4a519e4d5058d2
Jake Mulley <mulley.jake@me.com>
2016-04-24 21:06:48 +0100

    feature(tests): write mocha test

--- test.js
+++ test.js
@@ -1,8 +1,33 @@
-var expect     = require('chai').expect(),
-    injector   = require('./index');
+var expect     = require('chai').expect,
+    injector   = require('./index'),
+    fs         = require('fs');
 
-injector({
-  source: 'tests/expected.html',
-  basePaths: ['tests/_css', 'tests/_js'],
-  omit: 'tests'
+var testFile = 'tests/index.html';
+var expectedFile = 'tests/expected.html';
+
+describe('AssetInjector', function() {
+
+  before(function(done) {
+
+    injector({
+      source: testFile,
+      basePaths: ['tests/_css', 'tests/_js'],
+      omit: 'tests'
+    });
+
+    setTimeout(function() {
+      done();
+    }, 500);
+
+  });
+
+  it('should inject files correctly - compare against expected.html', function() {
+
+    var testFileSource = fs.readFileSync(testFile);
+    var expectedFileSource = fs.readFileSync(expectedFile);
+
+    expect(testFileSource.toString()).to.equal(expectedFileSource.toString());
+
+  });
+
 });
